3131f3229676a5bbcdd1a03e2931bb47c5a75110
Pedro Gonzalez <pdro74@hotmail.com>
2023-03-08 15:12:03 +0100

    Initial Commit

---
+++ StoryPrompt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryPrompt
+//
+//  Created by Pedro Gonzalez Ferrandez on 08.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
