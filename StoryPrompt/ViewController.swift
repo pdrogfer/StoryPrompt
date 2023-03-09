905485ca6491ec5a87b63d3318ba3f5ec7b58063
Pedro Gonzalez <pdro74@hotmail.com>
2023-03-09 16:36:54 +0100

    add UI fields and link to ViewController

--- StoryPrompt/ViewController.swift
+++ StoryPrompt/ViewController.swift
@@ -8,18 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nounTextField: UITextField!
+    @IBOutlet weak var adjectiveTextField: UITextField!
+    @IBOutlet weak var verbTextField: UITextField!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var numberSliderField: UISlider!
+    
+    let storyPrompt = StoryPromptEntry()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let storyPrompt = StoryPromptEntry()
+        numberSliderField.value = 7.5
         storyPrompt.noun = "noun"
         storyPrompt.adjective = "adjective"
         storyPrompt.verb = "verb"
-        storyPrompt.number = 10
+        storyPrompt.number = Int(numberSliderField.value)
         print(storyPrompt)
     }
-
-
+    
+    @IBAction func changeNumber(_ sender: UISlider) {
+        let newIntValue = Int(sender.value)
+        numberLabel.text = "Number: \(newIntValue)"
+        
+        storyPrompt.number = newIntValue
+        
+        print(storyPrompt)
+    }
+    
+    @IBAction func changeStoryType(_ sender: UISegmentedControl) {
+        if let genre = StoryPrompts.Genre(rawValue: sender.selectedSegmentIndex) {
+            storyPrompt.genre = genre
+        } else {
+            storyPrompt.genre = .scifi
+        }
+    }
 }
 
